9b1230f4470c81211079d227ed2b12a0252189f3
DDangle <101912067+DDangle@users.noreply.github.com>
2022-06-18 21:32:42 -0700

    code commit

---
+++ Contents.swift
@@ -0,0 +1,131 @@
+
+import UIKit
+
+//func bPlus(a: Int, b: Int) -> Int {
+//
+//    let plus = a + b
+//
+//    return plus
+//
+//}
+//let b = bPlus(a: 2, b: 5)
+//print(b)
+
+//Int
+//더하기
+func intPlus(a: Int, b: Int) {
+    
+    let plusResult = a + b
+    print(plusResult)
+    
+}
+intPlus(a: 1, b: 3)
+
+
+//빼기
+func intminus(c: Int, d: Int) {
+    
+    let minusResult = c - d
+    print(minusResult)
+}
+intminus(c: 6, d: 3)
+
+
+//곱하기
+func multiply(m1: Int, m2: Int) {
+    
+    let multiplyResult = m1 * m2
+    print(multiplyResult)
+}
+multiply(m1: 12, m2: 3)
+
+
+//나누기
+func divide(d1: Int, d2: Int) {
+    
+    let divideResult = d1 / d2
+    print(divideResult)
+}
+divide(d1: 12, d2: 6)
+
+//제곱
+func squared(q1: Int) {
+    let squaredResult = q1 * q1
+    print(squaredResult)
+}
+squared(q1: 10)
+
+//세개 더하기
+func threetimesPlus(melon: Int, banana: Int, apple: Int) {
+    let timeResult = melon + banana + apple
+    print(timeResult)
+}
+threetimesPlus(melon: 3, banana: 6, apple: 9)
+
+//세개 곱하기
+func threetimesMultiply(chco: Int, banila: Int, lemon: Int) {
+    let timeToResult = chco * banila * lemon
+    print(timeToResult)
+}
+threetimesMultiply(chco: 2, banila: 4, lemon: 6)
+
+
+
+
+// --- Double ---
+//더하기
+func doublePlus(a1: Double, b1: Double) {
+    
+    let dplusResult = a1 + b1
+    print(dplusResult)
+    
+}
+doublePlus(a1: 7.5, b1: 2.7)
+
+
+//빼기
+func doubleminus(c2: Double, d2: Double) {
+    
+    let dminusResult = c2 - d2
+    print(dminusResult)
+}
+doubleminus(c2: 5.7, d2: 3.4)
+
+
+//곱하기
+func doublemultiply(mm1: Double, mm2: Double) {
+    
+    let dmultiplyResult = mm1 * mm2
+    print(dmultiplyResult)
+}
+doublemultiply(mm1: 0.25, mm2: 0.25)
+
+
+//나누기
+func doubledivide(d11: Double, d21: Double) {
+    
+    let ddivideResult = d11 / d21
+    print(ddivideResult)
+}
+doubledivide(d11: 0.5, d21: 0.2)
+
+//제곱
+func doublesquared(qq1: Double) {
+    let dsquaredResult = qq1 * qq1
+    print(dsquaredResult)
+}
+doublesquared(qq1: 34.5)
+
+//세개 더하기
+func doublethreetimesPlus(melon: Double, banana: Double, apple: Double) {
+    let dtimeResult = melon + banana + apple
+    print(dtimeResult)
+}
+doublethreetimesPlus(melon: 1.5, banana: 4.2, apple: 7.4)
+
+//세개 곱하기
+func doublethreetimesMultiply(chco: Double, banila: Double, lemon: Double) {
+    let dtimeToResult = chco * banila * lemon
+    print(dtimeToResult)
+}
+doublethreetimesMultiply(chco: 4.2, banila: 5.5, lemon: 8.7)
